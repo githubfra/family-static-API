ec46a67eb22f762858cc2479148bd79666847fae
githubfra <diazcabellofrancisca@gmail.com>
2023-01-05 00:23:46 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
+import json
 #from models import Person
 
 app = Flask(__name__)
@@ -27,18 +28,40 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
         "hello": "world",
         "family": members
     }
+    return jsonify(members), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def list_single_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def add_jackson():
+    print(request.data)
+    data = request.json
+    print(data)
+    members = jackson_family.add_member(data)
+    if members == "added":
+        return jsonify('The new Jackson is here!'), 200
+    else:
+        return jsonify("That guy is not a Jackson's family member"), 400
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_jackson(id):
+    members = jackson_family.delete_member(id)
+    print(members)
+    if members == "not found":
+        return jsonify('This Jackson probably is not here anymore'), 400
+    else: 
+        return jsonify(members), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+
